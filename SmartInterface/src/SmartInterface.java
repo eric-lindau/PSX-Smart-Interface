e0cf05d7a6b04e80d4dde46c7d9279eea45022bc
Eric Lindau <eric.lindau1@gmail.com>
2017-06-27 01:12:07 -0600

    Very basic UI

--- SmartInterface/src/SmartInterface.java
+++ SmartInterface/src/SmartInterface.java
@@ -4,14 +4,18 @@ import net.java.games.input.Component;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Primary class that handles client creation, UI, calculations, and
  * server interaction.
  *
  * @author Eric Lindau
- * @version 1.0
+ * @version 0.0
  */
 public class SmartInterface {
 
@@ -31,24 +35,55 @@ public class SmartInterface {
         Client client = new Client("localhost", 10747);
 
         // Get a list of all recognized system controllers
-        Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
-        for(int i=0; i<controllers.length; i++)
-            // Print each controller's components
-            printComponents(controllers[i]);
+        ArrayList<Controller> controllers = new ArrayList<>
+                (Arrays.asList(ControllerEnvironment.getDefaultEnvironment()
+                        .getControllers()));
+        for(int i = 0; i < controllers.size(); i++)
+            if(controllers.get(i).getName().toUpperCase().contains("KEYBOARD") ||
+                    controllers.get(i).getName().toUpperCase().contains("MOUSE")) {
+                controllers.remove(i);
+                i--;
+            }
 
         // UI INITIALIZATION START
+        int components = 0;
+        for(Controller controller : controllers)
+            for(Component component : controller.getComponents())
+                components++;
+        GridLayout grid = new GridLayout(components, 4, 10, 0);
         JPanel panel = new JPanel();
+        panel.setLayout(grid);
+        panel.setPreferredSize(new Dimension(800, components*20));
+        JLabel label;
+        int counter = 0;
+        for(Controller controller : controllers) {
+            for(Component component : controller.getComponents()) {
+                label = new JLabel("  " + Integer.toString(counter) + ". " + component.getName());
+                panel.add(label);
+                counter++;
+            }
+        }
+
+        JScrollPane scrollPane = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setPreferredSize(new Dimension(800, 569));
 
         JFrame frame = new JFrame("PSX Smart SmartInterface");
         frame.setPreferredSize(new Dimension(800, 600));
+        frame.setLayout(new FlowLayout());
+        frame.setResizable(false);
         frame.setLocation(600, 100);
-        frame.getContentPane().add(panel);
+        frame.getContentPane().add(scrollPane);
         frame.pack();
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
         frame.setVisible(true);
         // UI INITIALIZATION END
 
         // TO BE REMOVED LATER
-        System.exit(0);
+        //System.exit(0);
     }
 
 }
